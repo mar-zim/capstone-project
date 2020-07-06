6862645ff3e4a90df5964929ed9441b7366b3bcd
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-06 12:45:47 +0200

    Initial setup and cleanup of app

---
+++ src/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+* {
+        box-sizing: border-box;
+    }
+
+body {
+	margin: 0;
+	background-color: #FFFCF9;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+}
+`
