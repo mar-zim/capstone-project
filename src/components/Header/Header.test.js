d72c432790837639f0546ed3169993e010a2af33
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-14 12:52:32 +0200

    Rewrite Header test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,10 @@
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
 import { MemoryRouter } from 'react-router-dom'
 import Header from './Header'
 
-it('renders the logo', () => {
-  const testLogo = 'logo'
-  render(<Header>{testLogo}</Header>, { wrapper: MemoryRouter })
-  expect(screen.getByAltText('logo')).toBeInTheDocument
+it('renders the logo as an image', () => {
+  const { queryByAltText } = render(<Header />, { wrapper: MemoryRouter })
+  expect(queryByAltText('logo').tagName).toBe('IMG')
 })
