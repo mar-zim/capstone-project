5e15f76c1a2f99e881005dade9cc321da52dacf9
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-17 14:01:54 +0200

    Implement react Spring

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ import deleteIcon from '../../icons/delete.svg'
 import PropTypes from 'prop-types'
 import cross from '../../icons/cross.svg'
 import searchIcon from '../../icons/search.svg'
+import { useSpring, animated } from 'react-spring'
 
 SearchBar.propTypes = {
   searchInput: PropTypes.string,
@@ -35,47 +36,50 @@ export default function SearchBar({
     setSearchTerm('')
   }
 
+  const animateWidth = useSpring({
+    width: isSearchBarVisible ? '300px' : '0px',
+  })
+
   return (
-    <>
+    <StyledSearchBar>
       {isSearchBarVisible ? (
-        <StyledSearchBar>
-          <ToggleIcon src={cross} alt="cross" onClick={endSearch} />
-          <StyledSearchForm onSubmit={(event) => event.preventDefault()}>
-            <StyledTextField
-              type="text"
-              placeholder="Wonach suchst du?"
-              value={searchInput}
-              onChange={handleSearch}
-              data-testid="textField"
-            />
-            <DeleteTextIcon
-              onClick={clearSearchField}
-              src={deleteIcon}
-              alt="delete"
-            />
-          </StyledSearchForm>
-        </StyledSearchBar>
+        <ToggleIcon src={cross} alt="cross" onClick={endSearch} />
       ) : (
-        <StyledSearchBar>
-          <ToggleIcon src={searchIcon} alt="searchIcon" onClick={viewSearch} />
-        </StyledSearchBar>
+        <ToggleIcon src={searchIcon} alt="searchIcon" onClick={viewSearch} />
       )}
-    </>
+      <StyledSearchForm
+        style={animateWidth}
+        onSubmit={(event) => event.preventDefault()}
+      >
+        <StyledTextField
+          type="text"
+          placeholder="Wonach suchst du?"
+          value={searchInput}
+          onChange={handleSearch}
+          data-testid="textField"
+        />
+        <DeleteTextIcon
+          onClick={clearSearchField}
+          src={deleteIcon}
+          alt="delete"
+        />
+      </StyledSearchForm>
+    </StyledSearchBar>
   )
 }
 
 const StyledSearchBar = styled.div`
   display: flex;
-  justify-content: space-between;
-  gap: 10px;
+  justify-content: left;
   align-items: center;
   padding: 10px 0;
 `
 
-const StyledSearchForm = styled.form`
+const StyledSearchForm = styled(animated.form)`
   position: relative;
+  margin-left: -10px;
   height: 35px;
-  width: 90%;
+  max-width: 90%;
   border-radius: 5px;
   border: 1px solid var(--grey-5);
 `
@@ -84,7 +88,7 @@ const StyledTextField = styled.input`
   width: 100%;
   border: none;
   border-radius: 5px;
-  padding-left: 10px;
+  padding-left: 20px;
   padding-right: 20%;
   font-family: var(--fontbody);
   font-size: 14px;
@@ -104,4 +108,5 @@ const DeleteTextIcon = styled.img`
 const ToggleIcon = styled.img`
   height: 35px;
   cursor: pointer;
+  z-index: 100;
 `
