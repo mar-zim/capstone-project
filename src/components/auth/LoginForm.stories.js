2ed62a82c24bdececb2344e2ee54a6e3c5ed45cf
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-23 20:34:15 +0200

    Add Stories for Auth Form

---
+++ src/components/auth/LoginForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import LoginForm from './LoginForm'
+
+export default {
+  component: LoginForm,
+  title: 'LoginForm',
+}
+
+export const Default = () => <LoginForm />
---
+++ src/components/auth/RegisterForm.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import RegisterForm from './RegisterForm'
+
+export default {
+  component: RegisterForm,
+  title: 'RegisterForm',
+}
+
+export const Default = () => <RegisterForm />
