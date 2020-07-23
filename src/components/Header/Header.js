5c28f4a59c89faa7dc7bfed71fc196b2b962f573
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-23 12:42:50 +0200

    Style Header with Grid

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import LogoutButton from '../auth/LogoutButton'
 import loginContext from '../../services/loginContext'
 import Button from '../Button/Button'
+import userLogo from '../../icons/user.svg'
 
 export default function Header() {
   const { user } = useContext(loginContext)
@@ -11,24 +12,33 @@ export default function Header() {
   return (
     <StyledHeader>
       <StyledLogo src={process.env.PUBLIC_URL + '/logo.svg'} alt="logo" />
-      {user ? (
+      <UserIcon src={userLogo} alt="user" />
+      {/* {user ? (
         <LogoutButton />
       ) : (
         <Link to="/welcome">
-          <Button text="login" />
+          <Button backColor="var(--lightblue)" text="login" />
         </Link>
-      )}
+      )} */}
     </StyledHeader>
   )
 }
 
 const StyledHeader = styled.header`
   background-color: var(--grey-5);
-  display: flex;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 70px auto 70px;
   align-items: center;
-  justify-content: space-around;
+  justify-items: center;
 `
 
 const StyledLogo = styled.img`
   max-width: 80vw;
+  grid-column: 2 / span 1;
+`
+
+const UserIcon = styled.img`
+  width: 40px;
+  grid-column: 3 / span 1;
 `
