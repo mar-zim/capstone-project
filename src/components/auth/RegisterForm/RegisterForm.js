c61b1e5df8bdef807932d889ec059348aedf4e7c
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-24 11:18:56 +0200

    Redirect to Home from register

--- src/components/auth/RegisterForm/RegisterForm.js
+++ src/components/auth/RegisterForm/RegisterForm.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import firebaseApp from '../../../firebase'
 import useForm from '../../../services/useForm'
 import Button from '../../Button/Button'
 import TextInputField from '../../TextInputField/TextInputField'
 
 export default function RegisterForm() {
-  const [isRegistered, setIsRegistered] = useState(false)
+  const history = useHistory()
   const { values, handleChange, handleSubmit } = useForm(registerToFirebase)
 
   async function registerToFirebase(values) {
@@ -17,51 +17,47 @@ export default function RegisterForm() {
     await newUser.user.updateProfile({
       displayName: values.name,
     })
-    return setIsRegistered(true)
+    goToHome()
   }
 
   return (
-    <>
-      {isRegistered ? (
-        <p>
-          Du bist angemeldet und kannst jetzt loslegen!{' '}
-          <Link to="/home">Home</Link>
-        </p>
-      ) : (
-        <div>
-          <h2>Registrierung</h2>
-          <form onSubmit={handleSubmit}>
-            <TextInputField
-              label="Dein User-Name"
-              name="name"
-              type="text"
-              handleChange={handleChange}
-              value={values.name || ''}
-              required={true}
-            />
-            <TextInputField
-              label="E-Mail"
-              name="email"
-              type="email"
-              handleChange={handleChange}
-              value={values.email || ''}
-              required={true}
-            />
-            <TextInputField
-              label="Passwort"
-              name="password"
-              type="password"
-              handleChange={handleChange}
-              value={values.password || ''}
-              required={true}
-            />
-            <Button text="registrieren" />
-          </form>
-          <div className="caption">
-            Zurück zum <Link to="/login">Login</Link>.
-          </div>
-        </div>
-      )}
-    </>
+    <div>
+      <h2>Registrierung</h2>
+      <form onSubmit={handleSubmit}>
+        <TextInputField
+          label="Dein User-Name"
+          name="name"
+          type="text"
+          handleChange={handleChange}
+          value={values.name || ''}
+          required={true}
+        />
+        <TextInputField
+          label="E-Mail"
+          name="email"
+          type="email"
+          handleChange={handleChange}
+          value={values.email || ''}
+          required={true}
+        />
+        <TextInputField
+          label="Passwort"
+          name="password"
+          type="password"
+          handleChange={handleChange}
+          value={values.password || ''}
+          required={true}
+        />
+        <Button text="registrieren" />
+      </form>
+      <div className="caption">
+        Zurück zum <Link to="/login">Login</Link>.
+      </div>
+    </div>
   )
+
+  function goToHome() {
+    let path = `/home`
+    history.push(path)
+  }
 }
