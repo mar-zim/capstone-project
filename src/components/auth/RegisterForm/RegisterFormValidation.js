824e3c5d8725a520edf32ec649e1eb7711017db3
mar-zim <mariuccia.zimmermann@gmx.de>
2020-07-25 17:16:25 +0200

    Update RegisterFormValidation

--- src/components/auth/RegisterForm/RegisterFormValidation.js
+++ src/components/auth/RegisterForm/RegisterFormValidation.js
@@ -1,23 +1,17 @@
 export default function validateRegister(values) {
   let errors = {}
 
-  if (!values.name) {
-    errors.name = 'Bitte gib einen Namen ein.'
-  } else if (values.name.length < 3) {
+  if (values.name && values.name.length < 3) {
     errors.name = 'Dein Name ist etwas zu kurz.'
-  } else if (values.name.length > 15) {
+  } else if (values.name && values.name.length > 15) {
     errors.name = 'Bitte wähle einen etwas kürzeren Namen.'
   }
 
-  if (!values.email) {
-    errors.email = 'Bitte gib eine E-Mail Adresse ein.'
-  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+  if (values.email && !/\S+@\S+\.\S+/.test(values.email)) {
     errors.email = 'Die E-Mail Adresse ist ungültig.'
   }
 
-  if (!values.password) {
-    errors.password = 'Bitte gib ein Passwort ein.'
-  } else if (values.password.length < 6) {
+  if (values.password && values.password.length < 6) {
     errors.password = 'Dein Passwort sollte mindestens 6 Zeichen lang sein!'
   }
 
