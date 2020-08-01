527159ddbcfe51830c400f04fab79b998e1a0bcf
mar-zim <mariuccia.zimmermann@gmx.de>
2020-08-01 14:23:43 +0200

    Make modal visible in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import Header from '../src/components/Header/Header'
@@ -13,16 +13,34 @@ import AddProductPage from './pages/AddProductPage'
 import useProductsFromFirestore from './services/useProductsFromFirestore'
 import Modal from './components/Modal/Modal'
 import NotFoundPage from './pages/NotFoundPage'
+import { animated, useTransition } from 'react-spring'
 
 function App() {
   const [user, userIsLoading] = useAuth()
   const [products, productsAreLoading] = useProductsFromFirestore()
+  const [modalVisible, setModalVisible] = useState(false)
+  const transitions = useTransition(modalVisible, null, {
+    from: { opacity: 0, transform: 'translateY(-40px)' },
+    enter: { opacity: 1, transform: 'translateY(0px)' },
+    leave: { opacity: 0, transform: 'translateY(-40px)' },
+  })
 
   return (
     <loginContext.Provider value={{ user, userIsLoading, firebaseApp }}>
       <AppWrapper>
         <Header />
         <StyledMain>
+          <button onClick={() => setModalVisible(true)}>Show modal</button>
+          {transitions.map(
+            ({ item, key, props: style }) =>
+              item && (
+                <Modal
+                  style={style}
+                  closeModal={() => setModalVisible(false)}
+                  key={key}
+                />
+              )
+          )}
           <Switch>
             <Redirect exact from="/" to="/home" />
             <Route path="/login" component={LoginPage} />
@@ -36,7 +54,7 @@ function App() {
                 />
               )}
             />
-            <Route path="/add" component={AddProductPage} />
+            {user && <Route path="/add" component={AddProductPage} />}
             <Route
               path="/details/:productId"
               component={() => (
